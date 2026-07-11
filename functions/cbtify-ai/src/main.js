e70f43206696bb13a3c659296ae2e21d4f83d50f
Harsh Kumar <harshrajputt23@gmail.com>
2026-07-11 11:17:14 +0530

    REST Gemini

--- functions/cbtify-ai/src/main.js
+++ functions/cbtify-ai/src/main.js
@@ -1,38 +1,42 @@
-import { GoogleGenAI } from "@google/genai";
-
-export default async ({ req, res, log, error, env }) => {
+export default async ({ req, res }) => {
   try {
-    log("Function Started");
-
     const apiKey = process.env.GEMINI_API_KEY;
 
     if (!apiKey) {
-      throw new Error("GEMINI_API_KEY not found");
+      throw new Error("GEMINI_API_KEY Missing");
     }
-
-    const ai = new GoogleGenAI({
-      apiKey,
-    });
 
     const body = req.body ? JSON.parse(req.body) : {};
 
     const prompt = body.prompt || "Hello";
 
-    const result = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: prompt,
-    });
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: prompt,
+                },
+              ],
+            },
+          ],
+        }),
+      },
+    );
 
-    return res.json({
-      success: true,
-      response: result.text,
-    });
-  } catch (err) {
-    error(err);
+    const data = await response.json();
 
+    return res.json(data);
+  } catch (e) {
     return res.json({
-      success: false,
-      error: err.message,
+      error: e.message,
     });
   }
 };
